2e2613b7104c12f6c557a15c72c742b2cb7033c3
Doğukan Özder <105740666+dogukanzder@users.noreply.github.com>
2022-05-17 20:15:37 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace ZderTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MetinKutusu = new System.Windows.Forms.TextBox();
+            this.Baslat = new System.Windows.Forms.Button();
+            this.Ticker = new System.Windows.Forms.Timer(this.components);
+            this.Metin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MetinKutusu
+            // 
+            this.MetinKutusu.BackColor = System.Drawing.SystemColors.Desktop;
+            this.MetinKutusu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MetinKutusu.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.MetinKutusu.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.MetinKutusu.Location = new System.Drawing.Point(0, 0);
+            this.MetinKutusu.Name = "MetinKutusu";
+            this.MetinKutusu.Size = new System.Drawing.Size(380, 43);
+            this.MetinKutusu.TabIndex = 5;
+            this.MetinKutusu.Text = "*14:35";
+            this.MetinKutusu.Click += new System.EventHandler(this.MetinKutusu_Click);
+            this.MetinKutusu.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MetinKutusu_KeyDown);
+            this.MetinKutusu.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MetinKutusu_KeyPress);
+            // 
+            // Baslat
+            // 
+            this.Baslat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Baslat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Baslat.ForeColor = System.Drawing.Color.Aquamarine;
+            this.Baslat.Location = new System.Drawing.Point(119, 130);
+            this.Baslat.Name = "Baslat";
+            this.Baslat.Size = new System.Drawing.Size(125, 67);
+            this.Baslat.TabIndex = 6;
+            this.Baslat.Text = "Başlat";
+            this.Baslat.UseVisualStyleBackColor = true;
+            this.Baslat.Click += new System.EventHandler(this.Baslat_Click);
+            // 
+            // Ticker
+            // 
+            this.Ticker.Tick += new System.EventHandler(this.Ticker_Tick);
+            // 
+            // Metin
+            // 
+            this.Metin.AutoSize = true;
+            this.Metin.Font = new System.Drawing.Font("Yu Gothic Light", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Metin.Location = new System.Drawing.Point(63, 264);
+            this.Metin.Name = "Metin";
+            this.Metin.Size = new System.Drawing.Size(227, 44);
+            this.Metin.TabIndex = 8;
+            this.Metin.Text = "12:34:567890";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(380, 357);
+            this.Controls.Add(this.Metin);
+            this.Controls.Add(this.Baslat);
+            this.Controls.Add(this.MetinKutusu);
+            this.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(20, 43);
+            this.Name = "Form1";
+            this.Text = "ZderTimer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox MetinKutusu;
+        private System.Windows.Forms.Button Baslat;
+        private System.Windows.Forms.Timer Ticker;
+        private System.Windows.Forms.Label Metin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Timers;
+using System.Media;
+
+
+namespace ZderTimer
+{
+    public partial class Form1 : Form
+    {
+        public static Zaman zaman = new Zaman();
+        int kontrol = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.Manual;
+            foreach (var scrn in Screen.AllScreens)
+            {
+                if (scrn.Bounds.Contains(this.Location))
+                {
+                    this.Location = new Point(scrn.Bounds.Right - this.Width, scrn.Bounds.Top);
+                    return;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Ticker.Stop();
+            Ticker.Interval = 100;
+            Metin.Text = null;
+            MetinKutusu.ForeColor = Color.White;
+        }
+
+        private void SesCal(bool flip)
+        {
+            SoundPlayer ses = new SoundPlayer(@"C:\Users\ZderWindows\Music\timer.wav");
+            if (flip)
+                ses.Play();
+            else
+                ses.Stop();
+        }
+
+        private void MetinKutusu_Click(object sender, EventArgs e)
+        {
+            MetinKutusu.Text = null;
+        }
+
+        private void MetinKutusu_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                Baslat_Click(this, new EventArgs());
+            }
+        }
+
+        private void Baslat_Click(object sender, EventArgs e)
+        {
+            if (kontrol == 0)
+            {
+                kontrol = 1;
+                zaman = zaman.Hesapla(MetinKutusu.Text);
+                Baslat.Text = "Durdur";
+                MetinKutusu.Text = null;
+                SesCal(false);
+                Ticker.Start();
+            }
+            else
+            {
+                kontrol = 0;
+                Baslat.Text = "Başlat";
+                Ticker.Stop();
+                SesCal(false);
+            }
+        }
+
+        private void Ticker_Tick(object sender, EventArgs e)
+        {
+            if (zaman.saniye == -2)
+                Metin.Text = String.Format("{0}", DateTime.Parse(DateTime.Now.ToString()).Subtract(DateTime.Parse(zaman.vakit.ToString())));
+            else if (kontrol == 1 && zaman.vakit.ToString() == DateTime.Now.ToString())
+            {
+                SesCal(true);
+                kontrol = 0;
+                Baslat.Text = "Başlat";
+                
+                Metin.Text = null; 
+                Ticker.Stop();
+            }
+            else
+                Metin.Text = String.Format("{0}", DateTime.Parse(DateTime.Now.ToString()).Subtract(DateTime.Parse(zaman.vakit.ToString()))).Substring(1);
+        }
+
+        private void MetinKutusu_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ':'))
+            {
+                e.Handled = true;
+            }
+            */
+        }
+    }
+    public class Zaman
+    {
+        public int saat;
+        public int dakika;
+        public int saniye;
+        public DateTime vakit = new DateTime();
+        
+        public Zaman Hesapla(string metin)
+        {
+            Zaman zaman = new Zaman();
+            zaman.vakit = DateTime.Now;
+            
+            if (metin.Length != 0) //key press te kontrol ettirki : ve numaralar dışın yazmasın
+            {
+                if (metin.Substring(0, 1) == "*")
+                {
+                    metin = metin.Substring(1);
+                    switch (metin.Split(':').Length - 1)
+                    {
+                        case 0:
+                            if (metin.Length == 1)
+                                zaman.saat = Convert.ToInt32(metin.Substring(0, 1));
+                            else
+                                zaman.saat = Convert.ToInt32(metin.Substring(0, 2));
+                            zaman.dakika = 0;
+                            zaman.saniye = 0;
+                            break;
+                        case 1:
+                            if (metin.IndexOf(':') == 1)
+                                zaman.saat = Convert.ToInt32(metin.Substring(0, 1));
+                            else
+                                zaman.saat = Convert.ToInt32(metin.Substring(0, 2));
+                            metin = metin.Substring(metin.IndexOf(":") + 1);
+                            if (metin.Length == 1)
+                                zaman.dakika = Convert.ToInt32(metin.Substring(0, 1));
+                            else
+                                zaman.dakika = Convert.ToInt32(metin.Substring(0, 2));
+                            zaman.saniye = 0;
+                            break;
+                        case 2:
+                            if (metin.IndexOf(':') == 1)
+                                zaman.saat = Convert.ToInt32(metin.Substring(0, 1));
+                            else
+                                zaman.saat = Convert.ToInt32(metin.Substring(0, 2));
+                            metin = metin.Substring(metin.IndexOf(":") + 1);
+                            if (metin.IndexOf(':') == 1)
+                                zaman.dakika = Convert.ToInt32(metin.Substring(0, 1));
+                            else
+                                zaman.dakika = Convert.ToInt32(metin.Substring(0, 2));
+                            metin = metin.Substring(metin.IndexOf(":") + 1);
+                            if (metin.Length == 1)
+                                zaman.saniye = Convert.ToInt32(metin.Substring(0, 1));
+                            else
+                                zaman.saniye = Convert.ToInt32(metin.Substring(0, 2));
+                            break;
+                        default:
+                            zaman.saat = -1;
+                            zaman.dakika = -1;
+                            zaman.saniye = -1;
+                            break;
+                    }
+                    zaman.vakit = new DateTime(zaman.vakit.Year, zaman.vakit.Month, zaman.vakit.Day, zaman.saat, zaman.dakika, zaman.saniye);
+                }
+                else if (metin.Last() == 'h')
+                {
+                    zaman.vakit = zaman.vakit.AddHours(Convert.ToInt32(metin.Remove(metin.Length - 1)));
+                }
+                else if (metin.Last() == 'm')
+                {
+                    zaman.vakit = zaman.vakit.AddMinutes(Convert.ToInt32(metin.Remove(metin.Length - 1)));
+                }
+                else if (metin.Last() == 's')
+                {
+                    zaman.vakit = zaman.vakit.AddSeconds(Convert.ToInt32(metin.Remove(metin.Length - 1)));
+                }
+                else if (metin == "sw")
+                {
+                    zaman.saniye = -2;
+                }
+            }
+            return zaman;
+        }
+    }
+
+    
+}
